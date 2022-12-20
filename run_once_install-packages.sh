b2a7d72ab82c41d2439ef884549b81b91f7d32df
Scott Callaway <github@scottcallaway.co.uk>
2022-12-20 11:42:30 +0000

    Fix packages script

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-sudo apt get update -y
-sudo apt get upgrade -y
-sudo apt get dist-upgrade -y
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt dist-upgrade -y
 
 # Install the basic dependencies
-sudo apt install software-properties-common zsh autojump jq nq unzip build-essentials ripgrep
+sudo apt install software-properties-common zsh autojump jq nq unzip ripgrep
 
 # Install rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
