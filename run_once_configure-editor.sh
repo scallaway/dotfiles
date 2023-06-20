93bfec432f06e3696d04548b068c3b6b1ee7462e
Scott Callaway <github@scottcallaway.co.uk>
2023-06-20 20:44:43 +0100

    Update run_once_configure-editor.sh

--- run_once_configure-editor.sh
+++ run_once_configure-editor.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 # Install packer
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
