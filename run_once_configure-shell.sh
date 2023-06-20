2cfef8271b452f1950507bcc6645483ce04ed48a
Scott Callaway <scott.callaway@uipath.com>
2023-06-20 16:25:31 +0100

    Add a few more scripts and update packages

---
+++ run_once_configure-shell.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# ZSH Syntax Highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Zoxide (a better cd)
+curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -5,7 +5,20 @@ sudo apt upgrade -y
 sudo apt dist-upgrade -y
 
 # Install the basic dependencies
-sudo apt install software-properties-common zsh autojump jq nq unzip ripgrep
+sudo apt install 							\
+	software-properties-common                                      \
+	zsh                                                             \
+	autojump                                                        \
+	jq                                                              \
+	nq                                                              \
+	unzip                                                           \
+	ripgrep                                                         \
+	cmake                                                           \
+	ninja-build                                                     \
+	gettext                                                         \
+	curl                                                            \
+	fzf                                                             \
+	tmux
 
 # Install rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
