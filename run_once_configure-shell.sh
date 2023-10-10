bf8ac85c25ce5488ff715936ef1033a1d4d5587e
Scott Callaway <github@scottcallaway.co.uk>
2023-10-10 14:51:31 +0100

    Add ZSH plugins to shell setup

--- run_once_configure-shell.sh
+++ run_once_configure-shell.sh
@@ -2,3 +2,17 @@
 
 # Zoxide (a better cd)
 curl -sS https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | bash
+
+# Zsh Plugins
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+mkdir ~/zsh-plugins
+cd ~/zsh-plugins
+
+# Git plugin
+mkdir git
+cd git
+curl -o git.plugin.zsh https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/git/git.plugin.zsh
+
+# ZSH Syntax Highlighting
+cd ~/zsh-plugins
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
